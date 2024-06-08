84b0ef65eca3641a9eb1210653581cc458e1c3a2
Busaidi <busaidi.nexus@gmail.com>
2024-06-08 11:49:28 +0400

    edit login.html signup.html form overridden

--- mazoon_aluminum/urls.py
+++ mazoon_aluminum/urls.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
+from users.views import SignUpView, CustomLoginView  # Import the custom login view and sign-up view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace='social')),
-    path('i18n/', include('django.conf.urls.i18n')),  # Add this line
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),  # Use the custom login view
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),  # Use the sign-up view
     path('accounts/', include('users.urls')),
     path('blog/', include('blog.urls')),
 )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib import messages
+from django.contrib.auth.views import LoginView
 from django.utils.translation import gettext_lazy as _
 
 
@@ -11,6 +12,24 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['username'].widget.attrs.update({'class': 'form-control'})
+        form.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        form.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        return form
+
+
+class CustomLoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'registration/login.html'
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['username'].widget.attrs.update({'class': 'form-control'})
+        form.fields['password'].widget.attrs.update({'class': 'form-control'})
+        return form
 
 
 @login_required
